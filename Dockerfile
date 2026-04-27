bd3935d22275c68a8965e0234e1d329565aeeb0f
stmaan <learn.stmaan@mail.ru>
2026-04-27 12:07:47 +0000

    Fix: Add pip install flask to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM python:3.10-slim
 WORKDIR /app
+# Сначала копируем зависимости
+COPY requirements.txt .
+# Устанавливаем их
+RUN pip install --no-cache-dir -r requirements.txt
+# Только потом копируем код
 COPY app.py .
+EXPOSE 80
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import Flask
-import sys
-import datetime
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"""
-    <h1>Hello from CI/CD Pipeline!</h1>
-    <p><b>Status:</b> Running in Kubernetes (Kind)</p>
-    <p><b>Time:</b> {now}</p>
-    <p><b>Python version:</b> {sys.version}</p>
-    """
+    return f"<h1>Успех! CI/CD работает!</h1><p>Контейнер запущен в Kind.</p>"
 
 if __name__ == '__main__':
-    # Слушаем на порту 80 внутри контейнера
     app.run(host='0.0.0.0', port=80)
